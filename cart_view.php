40db5ad2a96edcc68807985d6c429b2bc2231ad1
Zafran <zraaeae@gmail.com>
2024-02-15 01:19:05 -0800

    Bug fix-Add to cart

--- cart_view.php
+++ cart_view.php
@@ -157,11 +157,11 @@ function getTotal(){
 <!-- Paypal Express -->
 <script>
 paypal.Button.render({
-    env: 'sandbox', // change for production if app is live,
+    env: 'production', // change for production if app is live,
 
 	client: {
-        sandbox:    'ASb1ZbVxG5ZFzCWLdYLi_d1-k5rmSjvBZhxP2etCxBKXaJHxPba13JJD_D3dTNriRbAv3Kp_72cgDvaZ',
-        //production: 'AaBHKJFEej4V6yaArjzSx9cuf-UYesQYKqynQVCdBlKuZKawDDzFyuQdidPOBSGEhWaNQnnvfzuFB9SM'
+        //sandbox:    'ASb1ZbVxG5ZFzCWLdYLi_d1-k5rmSjvBZhxP2etCxBKXaJHxPba13JJD_D3dTNriRbAv3Kp_72cgDvaZ',
+        production: 'AaBHKJFEej4V6yaArjzSx9cuf-UYesQYKqynQVCdBlKuZKawDDzFyuQdidPOBSGEhWaNQnnvfzuFB9SM'
     },
 
     commit: true, // Show a 'Pay Now' button
